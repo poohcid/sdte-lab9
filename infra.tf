475f1e63b52abfb820f26750a655629ff442d7a3
poohcid <Poohcidpoo@hotmail.com>
2021-03-30 14:40:56 +0000

    new aws account

--- infra.tf
+++ infra.tf
@@ -1,8 +1,6 @@
-# Target Provider is AWS at region ap-southeast-1 (Singapore)
 provider "aws" {
-  region  = "us-east-1"
+  region  = "ap-southeast-1"
 }
-# ECR
 resource "aws_ecr_repository" "lab9_image_repo" {
   name = "lab9_image_repo"
 }
